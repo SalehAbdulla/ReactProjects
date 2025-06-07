45573da6477bf0253e323498022578dd42b13428
SalehAbdulla <144586926+SalehAbdulla@users.noreply.github.com>
2025-06-07 17:39:49 +0300

    I'm The One

---
+++ DaisyUi/my-react-app/src/App.tsx
@@ -0,0 +1,18 @@
+import Button from "./components/Button";
+import Modal from "./components/Modal";
+import RadixComponent from "./components/RadixComponent";
+import ShadcnButton from "./components/ShadcnButton";
+
+const App = () => {
+  return (
+    <div>
+      <Button />
+      <Modal />
+      <RadixComponent />
+      <hr />
+      <ShadcnButton />
+    </div>
+  );
+};
+
+export default App;
