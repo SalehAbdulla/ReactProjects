d4c30d14734cfeabcf9b25d040a6650a77eb89be
SalehAbdulla <144586926+SalehAbdulla@users.noreply.github.com>
2025-06-07 03:47:17 +0300

    I'm The One

---
+++ TypeScriptWithReact/ReactWithTailwind/my-react-app/src/components/UserList.tsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+
+type UsersType = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+};
+
+const UserList = () => {
+  const [users, setUsers] = useState<UsersType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resposne = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        if (!Response.ok) throw new Error("Response was not ok!");
+
+        const result = await resposne.json();
+        setUsers(result);
+      } catch (error) {
+        setError(error instanceof Error ? error.message : "An Error Occured");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <h1>Loading ...</h1>;
+  if (error) return <h1>{`Error: ${error}`}</h1>;
+
+  return (
+    <table className="m-20">
+      <thead>
+        <tr>
+          <th className="pr-2">id</th>
+          <th className="pr-2">name</th>
+          <th className="pr-2">username</th>
+          <th className="pr-2">email</th>
+          <th className="pr-2">phone</th>
+        </tr>
+      </thead>
+      
+      <tbody className="">
+        {users.map((user, index) => (
+          <tr key={index}>
+            <td className="pr-9">{user.id}</td>
+            <td className="pr-9">{user.name}</td>
+            <td className="pr-9">{user.username}</td>
+            <td className="pr-9">{user.email}</td>
+            <td className="pr-9">{user.phone}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserList;
