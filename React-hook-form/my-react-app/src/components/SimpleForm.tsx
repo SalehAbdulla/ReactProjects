1a758c869240a5c130cfaf09872737667ff47ef5
SalehAbdulla <144586926+SalehAbdulla@users.noreply.github.com>
2025-06-08 21:19:56 +0300

    I'm The One

--- React-hook-form/my-react-app/src/components/SimpleForm.tsx
+++ React-hook-form/my-react-app/src/components/SimpleForm.tsx
@@ -1,7 +1,7 @@
 import { useForm, type SubmitHandler } from "react-hook-form";
 import "./SimpleForm.css";
 
-type FormType = {
+type DataFromType = {
   name: string;
   email: string;
   password: string;
@@ -12,42 +12,49 @@ const SimpleForm = () => {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm<FormType>();
+  } = useForm<DataFromType>();
 
-  const onSubmit: SubmitHandler<FormType> = (data) => {
+  const onSubmit: SubmitHandler<DataFromType> = (data) => {
     console.log(data);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="form-container">
-        <label htmlFor="name">name</label>
-        {/* ... register to tell useFrom react take care of this input */}
-        <input
-          type="text"
-          id="name"
-          placeholder="name"
-          {...register("name", { required: "This Field is required" })}
-        />
-        {errors.name && <p style={{ color: "red" }}>{errors.name.message}</p>}
+    <section>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-container">
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            placeholder="Please Enter Your name"
+            id="name"
+            {...register("name", { required: "Name Field is required" })}
+          />
+          {errors.name && <p style={{ color: "red" }}>{errors.name.message}</p>}
 
-        <label htmlFor="email">Email Address</label>
-        <input
-          type="email"
-          id="email"
-          placeholder="email"
-          {...register("email", {
-            required: "This Field is required",
-            pattern: {
-              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: "Invalid email address",
-            },
-          })}
-        />
-        {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
-        <button type="submit" disabled={isSubmitting}>{isSubmitting ? "Loading ..." : "Submit"}</button>
-      </div>
-    </form>
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Please Enter Your email"
+            {...register("email", {
+              required: "Email Field is required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "invalid email address",
+              },
+            })}
+          />
+          {errors.email && (
+            <p style={{ color: "red" }}>{errors.email.message}</p>
+          )}  
+        
+          <label htmlFor="password">Password: </label>
+          <input type="password" placeholder="Enter Your Password" {...register("password", {required: "Password is Required"})} />
+          {errors.password && <p style={{color: "red"}} >{errors.password.message}</p>}
+          <button type="submit">{isSubmitting ? "Loading ..." : "Submit"}</button>
+        </div>
+      </form>
+    </section>
   );
 };
 
