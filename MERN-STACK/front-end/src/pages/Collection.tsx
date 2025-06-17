5c12f003dd1887042df6bdf39ca1c712f50758ce
Saleh Abdulla <salehabdullaict@gmail.com>
2025-06-17 21:37:49 +0300

    Collection page - Filter full functionality added

--- MERN-STACK/front-end/src/pages/Collection.tsx
+++ MERN-STACK/front-end/src/pages/Collection.tsx
@@ -1,63 +1,196 @@
+import React, { useState, useEffect } from 'react'
 import { assets } from '../assets/frontend_assets/assets'
-import { useState } from 'react';
+import ProductItem from '../components/ProductItem'
+import Title from '../components/Title'
+import { UseShopContext } from '../context/UseShopContext'
+import type { ProductType } from '../context/ShopContext'
 
 const Collection = () => {
+  const { products } = UseShopContext()
+  const [showFilters, setShowFilters] = useState<boolean>(true)
+  const [filterProducts, setFilterProducts] = useState<ProductType[]>([])
+  const [priceFilter, setPriceFilter] = useState("relavent");
 
+  useEffect(() => {
+    setFilterProducts(products)
+  }, [products])
 
-  const [showFilters, setShowFilters] = useState<boolean>(true);
+  // ---- Categories onChange
+
+  const [categories, setCategories] = useState<string[]>([])
+  const toggleCategoryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    // If value already available, then remove it; otherwise add it.
+    setCategories((prev) =>
+      prev.includes(value)
+        ? prev.filter((item) => item != value)
+        : [...prev, value]
+    )
+  }
+
+  // Types onChange
+  const [types, setTypes] = useState<string[]>([])
+  const toggleTypesOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    setTypes((prev) =>
+      prev.includes(value)
+        ? prev.filter((item) => item !== value)
+        : [...prev, value]
+    )
+  }
+
+  //-------
+
+  // Apply Filter Function
+
+  const applyFilter = () => {
+    let productCopy = products.slice()
+    if (categories.length > 0) {
+      productCopy = productCopy.filter((item) =>
+        categories.includes(item.category)
+      )
+    }
+
+    if (types.length > 0) {
+      productCopy = productCopy.filter((item) =>
+        types.includes(item.subCategory)
+      )
+    }
+    setFilterProducts(productCopy)
+  }
+
+  useEffect(() => {
+    applyFilter()
+  }, [categories, types])
+
+  // Sort By Price
+
+  const sortByPrice = () => {
+
+    const copyProductsFiltered = filterProducts.slice();
+
+    switch (priceFilter) {
+      case 'low-high':
+        copyProductsFiltered.sort((a, b) => (a.price - b.price));
+        setFilterProducts(copyProductsFiltered);
+        break;
+      case 'high-low':
+        copyProductsFiltered.sort((a, b) => (b.price - a.price));
+        setFilterProducts(copyProductsFiltered);
+        break;
+      default:
+        applyFilter();
+        break;
+    }
+  }
+
+  useEffect(()=> {
+    sortByPrice();
+  },[priceFilter])
+
 
 
   return (
-    <section className="flex flex-col justify-center">
-
-      <div className="flex gap-3 justify-contenn items-center">
-        <p className="text-xl">FILTER</p>
-        <img
-          onClick={() => setShowFilters(!showFilters)}
-          src={assets.dropdown_icon}
-          alt="option buttom"
-          className={`w-2 h-5 cursor-pointer ${showFilters ? "rotate-90" : "rotate-0"} `}
-        />
-      </div>
-
-      <div className={`transition-all ${!showFilters ? "hidden" : ""}`}>
-        <br />
-        <div className="flex flex-col justify-center border-1 border-gray-300 gap-2 p-4 sm:w-1/3 w-full">
-          <h3 className="font-semibold">CATEGORY</h3>
-          <p className="flex gap-2">
-             <input className="w-3" type="checkbox" value={'Men'} /> Men
-          </p>
-
-          <p className="flex gap-2">
-             <input className="w-3" type="checkbox" value={'Women'} /> Women
-          </p>
-
-          <p className="flex gap-2">
-             <input className="w-3" type="checkbox" value={'Kids'} /> Kids
-          </p>
+    <div className="grid grid-cols-1 sm:grid-cols-[0.5fr_3fr] gap-5">
+      {/* Aside */}
+      <aside>
+        <div className="flex items-center gap-2 mt-5">
+          <p className="text-xl font-medium">FILTERS</p>
+          <img
+            onClick={() => setShowFilters(!showFilters)}
+            src={assets.dropdown_icon}
+            alt="drop down icon"
+            className={`w-3 cursor-pointer ${
+              !showFilters ? '' : 'rotate-90'
+            } mx-2`}
+          />
         </div>
 
-        <br />
+        <div className={`${showFilters ? '' : 'hidden'}`}>
+          <div className="border p-3 mt-5 flex flex-col gap-1 w-full border-gray-300">
+            <p>CATEGORIES</p>
+            <label>
+              <input
+                type="checkbox"
+                value="Men"
+                onChange={toggleCategoryChange}
+              />{' '}
+              Men
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="Women"
+                onChange={toggleCategoryChange}
+              />{' '}
+              Women
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="Kids"
+                onChange={toggleCategoryChange}
+              />{' '}
+              Kids
+            </label>
+          </div>
 
-        <div className="flex flex-col justify-center border-1 border-gray-300 gap-2 p-4 sm:w-1/3 w-full">
-          <h3 className="font-semibold">TYPE</h3>
+          <div className="border p-3 mt-5 flex flex-col gap-1 w-full border-gray-300">
+            <p>TYPE</p>
+            <label>
+              <input
+                type="checkbox"
+                value="Topwear"
+                onChange={toggleTypesOnChange}
+              />{' '}
+              Topwear
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="Bottomwear"
+                onChange={toggleTypesOnChange}
+              />{' '}
+              Bottomwear
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="Winterwear"
+                onChange={toggleTypesOnChange}
+              />{' '}
+              Winterwear
+            </label>
+          </div>
+        </div>
+      </aside>
 
-          <p className="flex gap-2">
-             <input className="w-3" type="checkbox" value={'Topwear'} /> Topwear
-          </p>
-
-          <p className="flex gap-2">
-             <input className="w-3" type="checkbox" value={'Bottomwear'} /> Bottomwear
-          </p>
-
-          <p className="flex gap-2">
-             <input className="w-3" type="checkbox" value={'Winterwear'} /> Winterwear
-          </p>
-
+      <div>
+        <div className="flex justify-between items-center mt-5">
+          <Title text1="ALL" text2="COLLECTIONS" />
+          <select
+            onChange={(event: React.ChangeEvent<HTMLSelectElement>) => setPriceFilter(event.target.value)}
+            className="p-2 border border-gray-300"
+          >
+            <option value="Relevant">Sort By: Relevant</option>
+            <option value="low-high">Sort By: Low to High</option>
+            <option value="high-low">Sort By: High to Low</option>
+          </select>
         </div>
 
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 mt-5">
+          {filterProducts?.map((item, index) => (
+            <ProductItem
+              key={index}
+              id={item._id}
+              name={item.name}
+              price={item.price}
+              image={item.image}
+            />
+          ))}
+        </div>
       </div>
-    </section>
+    </div>
   )
 }
 
